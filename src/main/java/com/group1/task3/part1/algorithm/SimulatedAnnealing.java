1847dc6cfe8811cc5ae19c2554ef55b879b856f0
ZuxingGit <1245766137@qq.com>
2024-04-24 21:14:40 +0930

    updated SimulatedAnnealing

--- src/main/java/com/group1/task3/part1/algorithm/SimulatedAnnealing.java
+++ src/main/java/com/group1/task3/part1/algorithm/SimulatedAnnealing.java
@@ -1,41 +1,3 @@
-// package com.group1.task3.part1.algorithm;
-
-// import java.util.Random;
-// import java.util.function.Function;
-
-// public class SimulatedAnnealing {
-//     private static final Random r = new Random();
-
-//     public static double search(Function<Double, Double> function, double lowerBound, double upperBound, int iterations, double initialTemp, double coolingRate) {
-//         double currentSolution = lowerBound + r.nextDouble() * (upperBound - lowerBound);
-//         double currentFitness = function.apply(currentSolution);
-
-//         double temperature = initialTemp;
-
-//         for (int i = 0; i < iterations; i++) {
-//             double newSolution = currentSolution + (r.nextDouble() - 0.5);
-//             double newFitness = function.apply(newSolution);
-
-//             if (shouldAccept(currentFitness, newFitness, temperature)) {
-//                 currentSolution = newSolution;
-//                 currentFitness = newFitness;
-//             }
-
-//             temperature *= 1 - coolingRate;
-//         }
-
-//         return currentSolution;
-//     }
-
-//     private static boolean shouldAccept(double currentFitness, double newFitness, double temperature) {
-//         if (newFitness < currentFitness) {
-//             return true;
-//         }
-//         double delta = newFitness - currentFitness;
-//         return Math.exp(-delta / temperature) > r.nextDouble();
-//     }
-// }
-
 package com.group1.task3.part1.algorithm;
 
 import java.awt.image.BufferedImage;
@@ -54,138 +16,142 @@ public class SimulatedAnnealing {
     // private static ArrayList<ArrayList<Integer>> currentSolution = new ArrayList<ArrayList<Integer>>();
     private static BufferedImage currentBestImage;
 
-    public static ArrayList<ArrayList<Integer>> simpleAppSearch(Function<ArrayList<ArrayList<Integer>>, Double> function, int iterations) {
+    public static ArrayList<ArrayList<Integer>> simpleAppSearch(Function<ArrayList<ArrayList<Integer>>, Double> function, 
+                                                int iterations, double initialTemperature, double coolingRate) {
         ArrayList<ArrayList<Integer>> currentSolution = new ArrayList<ArrayList<Integer>>();
-        ArrayList<ArrayList<Integer>> bestSolution = new ArrayList<ArrayList<Integer>>();
-        // currentSolution contains RGB values for each simpleApp GUI component.
+        double temperature = initialTemperature;
+        // currentSolution contains RGB values for each simpleApp GUI component. 
         // Only 6 components needed
-        for (int i=0;i<6;i++){
+        for (int i = 0; i < 6; i++) {
             currentSolution.add(new ArrayList<Integer>(Arrays.asList(new Integer[]{r.nextInt(256), r.nextInt(256), r.nextInt(256)})));
+            // 2. jTextField1 (start from 0)
+            // 3. jTextField1TextColor (start from 0)
         }
-        // ArrayList<ArrayList<Integer>> currentSolution = lowerBound + r.nextInt(upperBound - lowerBound) + 1;
+
         double currentFitness = function.apply(currentSolution);
-        double bestFitness = currentFitness;
         System.out.println("Initial solution: " + currentSolution + "\nInitial fitness: " + currentFitness);
         sa.saveScreenShot(null, "initial.png");
 
-        double temperature = 10000.0;
-        double coolingRate = 0.003;
-
         for (int i = 0; i < iterations; i++) {
-            if (temperature > 1.0) {
-                System.out.println("--------------------------------------------");
-                int delta = r.nextInt(51) - 25;//delta in range (-25~25)
-                System.out.println("Delta: " + delta);
-                ArrayList<ArrayList<Integer>> newSolution = generateNewSolution(delta, currentSolution);
-                double newFitness = function.apply(newSolution);
-                System.out.println("New solution: " + newSolution + "\nNew fitness: " + newFitness);
-
-                if (newFitness < currentFitness || Math.exp(bestFitness - newFitness) / temperature > r.nextDouble()) {
-                    currentSolution = newSolution;
-                    currentFitness = newFitness;
-                    currentBestImage = sa.imageContent;
-                    if (currentFitness< bestFitness) {
-                        bestSolution = currentSolution;
-                        bestFitness = currentFitness;
-                    }
-                }
-                temperature *= 1- coolingRate;
-                System.out.println("Current solution: " + currentSolution + "\nCurrent fitness: " + currentFitness);
-
-                if (i == 9) {
-                    sa.saveScreenShot(currentBestImage, "10.png");
-                    System.out.println("============================================");
-                    System.out.println("10th solution: " + currentSolution + "\n10th fitness: " + currentFitness);
-                } else if (i == 99) {
-                    sa.saveScreenShot(currentBestImage, "100.png");
-                    System.out.println("============================================");
-                    System.out.println("100th solution: " + currentSolution + "\n100th fitness: " + currentFitness);
-                } else if (i == iterations - 1) {
-                    if (i == 999) {
-                        sa.saveScreenShot(currentBestImage, "1000.png");
-                        System.out.println("============================================");
-                        System.out.println("1000th solution: " + currentSolution + "\n1000th fitness: " + currentFitness);
-                    } else {
-                        sa.saveScreenShot(currentBestImage, "final.png");
-                        System.out.println("============================================");
-                        System.out.println("Final solution: " + currentSolution + "\nFinal fitness: " + currentFitness);
-                    }
-                }
-
+            System.out.println("--------------------------------------------");
+            Integer value = r.nextInt(25) + 1; //value is between 1 and 25
+            Integer sign = (r.nextInt(2) == 0) ? -1 : 1; //delta is either -1 or 1
+            Integer delta = sign * value; //delta is between -25 and 25, 0 excluded
+            System.out.println("Delta: " + delta);
+            ArrayList<ArrayList<Integer>> newSolution = generateNewSolution(delta, currentSolution);
+            if (Euclidean.getDistance(newSolution.get(2), newSolution.get(3)) < 128) {
+                System.out.println("Distance between jTextField1 and jTextField1TextColor is less than 128");
+                // so that background color is black, the distance of text color between bg color is 128
+                newSolution.set(2, new ArrayList<Integer>(Arrays.asList(0, 0, 0)));
+                newSolution.set(3, new ArrayList<Integer>(Arrays.asList(74, 74, 74)));
             }
+            double newFitness = function.apply(newSolution);
+            System.out.println("New solution: " + newSolution + "\nNew fitness: " + newFitness);
+            // accept it or not
+            if (shouldAccept(currentFitness, newFitness, temperature)) {
+                currentSolution = newSolution;
+                currentFitness = newFitness;
+                currentBestImage = sa.imageContent;
+            }
+            System.out.println("Current solution: " + currentSolution + "\nCurrent fitness: " + currentFitness);
+
+            if (i == 9) {
+                sa.saveScreenShot(currentBestImage, "10.png");
+                System.out.println("============================================");
+                System.out.println("10th solution: " + currentSolution + "\n10th fitness: " + currentFitness);
+            } else if (i == 99) {
+                sa.saveScreenShot(currentBestImage, "100.png");
+                System.out.println("============================================");
+                System.out.println("100th solution: " + currentSolution + "\n100th fitness: " + currentFitness);
+            } else if (i == iterations-1) {
+                if (i == 999) {
+                    sa.saveScreenShot(currentBestImage, "1000.png");
+                    System.out.println("============================================");
+                    System.out.println("1000th solution: " + currentSolution + "\n1000th fitness: " + currentFitness);
+                } else {
+                    sa.saveScreenShot(currentBestImage, "final.png");
+                    System.out.println("============================================");
+                    System.out.println("Final solution: " + currentSolution + "\nFinal fitness: " + currentFitness);
+                }
+            }
+
+            temperature *= 1 - coolingRate;
         }
-        return bestSolution;
+        return currentSolution;
     }
 
-
-    public static ArrayList<ArrayList<Integer>> caculatorSearch(Function<ArrayList<ArrayList<Integer>>, Double> function, int iterations) {
+    public static ArrayList<ArrayList<Integer>> caculatorSearch(Function<ArrayList<ArrayList<Integer>>, Double> function, 
+                                                int iterations, double initialTemperature, double coolingRate) {
         ArrayList<ArrayList<Integer>> currentSolution = new ArrayList<ArrayList<Integer>>();
-        ArrayList<ArrayList<Integer>> bestSolution = new ArrayList<ArrayList<Integer>>();
-        // currentSolution contains RGB values for each caculator GUI component.
+        double temperature = initialTemperature;
+        // currentSolution contains RGB values for each caculator GUI component. 
         // All 27 components needed
-        for (int i=0;i<27;i++){
+        for (int i = 0; i < 27; i++) {
             currentSolution.add(new ArrayList<Integer>(Arrays.asList(new Integer[]{r.nextInt(256), r.nextInt(256), r.nextInt(256)})));
+            // 19. jTextField1 (start from 0)
+            // 20. jTextField1TextColor (start from 0)
         }
 
-        // ArrayList<ArrayList<Integer>> currentSolution = lowerBound + r.nextInt(upperBound - lowerBound) + 1;
         double currentFitness = function.apply(currentSolution);
-        double bestFitness = currentFitness;
         System.out.println("Initial solution: " + currentSolution + "\nInitial fitness: " + currentFitness);
         ca.saveScreenShot(null, "initial.png");
 
-        double temperature = 10000.0;
-        double coolingRate = 0.003;
-
         for (int i = 0; i < iterations; i++) {
-            if (temperature > 1.0) {
-                System.out.println("--------------------------------------------");
-                int delta = r.nextInt(51) - 25;//delta in range (-25~25)
-                System.out.println("Delta: " + delta);
-                ArrayList<ArrayList<Integer>> newSolution = generateNewSolution(delta, currentSolution);
-                double newFitness = function.apply(newSolution);
-                System.out.println("New solution: " + newSolution + "\nNew fitness: " + newFitness);
+            System.out.println("--------------------------------------------");
+            Integer value = r.nextInt(25) + 1; //value is between 1 and 25
+            Integer sign = (r.nextInt(2) == 0) ? -1 : 1; //delta is either -1 or 1
+            Integer delta = sign * value; //delta is between -25 and 25, 0 excluded
+            System.out.println("Delta: " + delta);
+            ArrayList<ArrayList<Integer>> newSolution = generateNewSolution(delta, currentSolution);
+            if (Euclidean.getDistance(newSolution.get(19), newSolution.get(20)) < 128) {
+                System.out.println("Distance between jTextField1 and jTextField1TextColor is less than 128");
+                // so that background color is black, the distance of text color between bg color is 128
+                newSolution.set(19, new ArrayList<Integer>(Arrays.asList(0, 0, 0)));
+                newSolution.set(20, new ArrayList<Integer>(Arrays.asList(74, 74, 74)));
+            }
+            double newFitness = function.apply(newSolution);
+            System.out.println("New solution: " + newSolution + "\nNew fitness: " + newFitness);
+            // accept it or not
+            if (shouldAccept(currentFitness, newFitness, temperature)) {
+                currentSolution = newSolution;
+                currentFitness = newFitness;
+                currentBestImage = ca.imageContent;
+            }
 
-                if (newFitness < currentFitness || Math.exp(bestFitness - newFitness) / temperature > r.nextDouble()) {
-                    currentSolution = newSolution;
-                    currentFitness = newFitness;
-                    currentBestImage = sa.imageContent;
-                    if (currentFitness < bestFitness) {
-                        bestSolution = currentSolution;
-                        bestFitness = currentFitness;
-                    }
-                }
-                temperature *= 1 - coolingRate;
-                System.out.println("Current solution: " + currentSolution + "\nCurrent fitness: " + currentFitness);
+            System.out.println("Current solution: " + currentSolution + "\nCurrent fitness: " + currentFitness);
 
-                if (i == 9) {
-                    ca.saveScreenShot(currentBestImage, "10.png");
+            if (i == 9) {
+                ca.saveScreenShot(currentBestImage, "10.png");
+                System.out.println("============================================");
+                System.out.println("10th solution: " + currentSolution + "\n10th fitness: " + currentFitness);
+            } else if (i == 99) {
+                ca.saveScreenShot(currentBestImage, "100.png");
+                System.out.println("============================================");
+                System.out.println("100th solution: " + currentSolution + "\n100th fitness: " + currentFitness);
+            } else if (i == iterations-1) {
+                if (i == 999) {
+                    ca.saveScreenShot(currentBestImage, "1000.png");
                     System.out.println("============================================");
-                    System.out.println("10th solution: " + currentSolution + "\n10th fitness: " + currentFitness);
-                } else if (i == 99) {
-                    ca.saveScreenShot(currentBestImage, "100.png");
+                    System.out.println("1000th solution: " + currentSolution + "\n1000th fitness: " + currentFitness);
+                } else {
+                    ca.saveScreenShot(currentBestImage, "final.png");
                     System.out.println("============================================");
-                    System.out.println("100th solution: " + currentSolution + "\n100th fitness: " + currentFitness);
-                } else if (i == iterations - 1) {
-                    if (i == 999) {
-                        ca.saveScreenShot(currentBestImage, "1000.png");
-                        System.out.println("============================================");
-                        System.out.println("1000th solution: " + currentSolution + "\n1000th fitness: " + currentFitness);
-                    } else {
-                        ca.saveScreenShot(currentBestImage, "final.png");
-                        System.out.println("============================================");
-                        System.out.println("Final solution: " + currentSolution + "\nFinal fitness: " + currentFitness);
-                    }
+                    System.out.println("Final solution: " + currentSolution + "\nFinal fitness: " + currentFitness);
                 }
             }
+            temperature *= 1 - coolingRate;
         }
-        return bestSolution;
+
+        return currentSolution;
     }
 
     /**
      * Generate new solution based on the current solution and delta
-
+     * @param delta
+     * @param currentSolution
+     * @return new solution
      */
-    public static ArrayList<ArrayList<Integer>> generateNewSolution(int delta, ArrayList<ArrayList<Integer>> currentSolution) {
+    public static ArrayList<ArrayList<Integer>> generateNewSolution(Integer delta, ArrayList<ArrayList<Integer>> currentSolution) {
         ArrayList<ArrayList<Integer>> newSolution = new ArrayList<ArrayList<Integer>>();
         for (int i = 0; i < currentSolution.size(); i++) {
             ArrayList<Integer> newRGB = new ArrayList<Integer>();
@@ -203,10 +169,26 @@ public class SimulatedAnnealing {
         return newSolution;
     }
 
-    public static void main(String[] args) {
-        simpleAppSearch(currentSolution -> sa.runApp(currentSolution), 10);
-        // caculatorSearch(currentSolution -> ca.runApp(currentSolution), 10);
-
+    /**
+     * Check if the new solution should be accepted
+     * @param currentFitness
+     * @param newFitness
+     * @param temperature
+     * @return true if the new solution should be accepted, false otherwise
+     */
+    private static boolean shouldAccept(double currentFitness, double newFitness, double temperature) {
+        if (newFitness < currentFitness) {
+            return true;
+        }
+        double delta = newFitness - currentFitness;
+        return Math.exp(-delta / temperature) > r.nextDouble();
     }
 
+    public static void main(String[] args) {
+        double temperature = 1000;
+        double coolingRate = 0.003;
+
+        simpleAppSearch(currentSolution -> sa.runApp(currentSolution), 10, temperature, coolingRate);
+        caculatorSearch(currentSolution -> ca.runApp(currentSolution), 10, temperature, coolingRate);
+    }
 }
