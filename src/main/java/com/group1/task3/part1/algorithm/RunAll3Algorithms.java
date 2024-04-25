bdda1fbc207e1c7fce03148070108273d1989ffe
ZuxingGit <1245766137@qq.com>
2024-04-25 17:59:20 +0930

    run all 3

---
+++ src/main/java/com/group1/task3/part1/algorithm/RunAll3Algorithms.java
@@ -0,0 +1,9 @@
+package com.group1.task3.part1.algorithm;
+
+public class RunAll3Algorithms {
+    public static void main(String[] args) {
+        RandomSearch.main(args);
+        HillClimbing.main(args);
+        SimulatedAnnealing.main(args);
+    }
+}
