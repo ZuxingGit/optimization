a8a9ce893c27c380028e9adb3915305f7d08bbfb
ZuxingGit <1245766137@qq.com>
2024-04-27 22:26:05 +0930

    Euclidean method sum

--- src/main/java/com/group1/task3/part1/algorithm/Euclidean.java
+++ src/main/java/com/group1/task3/part1/algorithm/Euclidean.java
@@ -1,5 +1,6 @@
 package com.group1.task3.part1.algorithm;
 
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 /**
@@ -21,4 +22,33 @@ public class Euclidean {
 
         return Math.sqrt(redDistance * redDistance + greenDistance * greenDistance + blueDistance * blueDistance);
     }
+
+    /**
+	 * Calculate the Euclidean distance between two solutions
+	 * @param referenceImage
+	 * @param currentImage
+	 * @return the sum of Euclidean colour differences for each pixel of two images
+	 */
+	public static double getEuclideanDistanceSum(BufferedImage refImage, BufferedImage currentImage) {
+		double distance = 0;
+		int height = refImage.getHeight();
+		int width = refImage.getWidth();
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				int refRGB = refImage.getRGB(x, y);
+				int currentRGB = currentImage.getRGB(x, y);
+				int refRed = (refRGB >> 16) & 0xff;
+				int refGreen = (refRGB >> 8) & 0xff;
+				int refBlue = refRGB & 0xff;
+				int currentRed = (currentRGB >> 16) & 0xff;
+				int currentGreen = (currentRGB >> 8) & 0xff;
+				int currentBlue = currentRGB & 0xff;
+				double d = Math.pow(refRed - currentRed, 2) + Math.pow(refGreen - currentGreen, 2) + Math.pow(refBlue - currentBlue, 2);
+				distance += Math.sqrt(d);
+			}
+		}
+		System.out.println("Total Euclidean distance: " + distance);
+
+		return distance;
+	}
 }
