5b8f49c14f754fb0d0e1fa622dbdb5ae8d10b0c9
starrain1031 <88185575+starrain1031@users.noreply.github.com>
2024-04-28 10:22:25 +0930

    task 3 part 2 plots

---
+++ src/main/java/com/group1/task3/part1/t3plots/t3plots/boxplotcompare2.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+algorithms = ["RandomSearch", "HillClimbing", "SimulatedAnnealing"]
+location = "screenshots/simpleApp/"
+data = []
+for alg in algorithms:
+    count = 1
+    shot_fitness = []
+    for i in range(3):
+        count *= 10
+        fitness = []
+        for j in range(10):
+            path = location + alg + "/round" + str(j+1) + "/" + str(count) + ".csv"
+            # print(path)
+            df = pd.read_csv(path)
+
+            val_1 = df.iloc[5, 1]
+            fitness.append(val_1)
+        shot_fitness.append(fitness)
+    data.append(shot_fitness)
+
+
+def bxplot(data,i):
+    plt.boxplot(data)
+
+    plt.xticks([1, 2, 3], algorithms)
+
+    plt.suptitle('Comparison of fitness for 3 algorithms')
+
+    plt.title('')
+    plt.xlabel('Algorithm')
+    plt.ylabel('Charge consumption')
+    num_of_shots = pow(10, i + 1)
+    plt.savefig(f'box_plot_comparison_sim{num_of_shots}.png')
+
+
+for i in range(3):
+    bxplot(data[i],i)
