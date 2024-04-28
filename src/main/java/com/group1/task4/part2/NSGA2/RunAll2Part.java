2b671062d7c419e32e31f9f0c4bdd23243e59efb
LWF15 <liuweifan1999@gmail.com>
2024-04-28 12:58:45 +0800

    Task4 part2 screenshot of Simple app

--- src/main/java/com/group1/task4/part2/NSGA2/RunAll2Part.java
+++ src/main/java/com/group1/task4/part2/NSGA2/RunAll2Part.java
@@ -2,7 +2,7 @@ package com.group1.task4.part2.NSGA2;
 
 public class RunAll2Part {
     public static void main(String[] args) {
-         Calculator_NSGA_II.main(args);
+        //  Calculator_NSGA_II.main(args);
          SimpleApp_NSGA_II.main(args);
     }
 }
